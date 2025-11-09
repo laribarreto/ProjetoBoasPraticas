65a1b49456c5ee259ffef1e129491cf25ea931ce
laribarreto <larissa.barreto.106@ufrn.edu.br>
2025-11-09 15:34:21 -0300

    Create LivroDigital.java

---
+++ src/item/LivroDigital.java
@@ -0,0 +1,32 @@
+package item;
+
+/**
+ * Representa um livro digital (sem limite de cópias).
+ */
+public class LivroDigital extends Item {
+
+    public LivroDigital(String titulo, String autor, String isbn) {
+        super(titulo, autor, isbn);
+    }
+
+    @Override
+    public boolean isDisponivel() {
+        return true;
+    }
+
+    @Override
+    public void emprestar() {
+        // Sempre disponível
+    }
+
+    @Override
+    public void devolver() {
+        // Nenhuma ação necessária
+    }
+
+    @Override
+    public void exibirInfo() {
+        super.exibirInfo();
+        System.out.println("Tipo: Digital | Sempre disponível");
+    }
+}
